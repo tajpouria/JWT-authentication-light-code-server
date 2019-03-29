ca27fd11c63ca2f730c05490bd4ff2d6ad2220d2
Pouria <charliswager@gmail.com>
2019-03-30 00:11:08 +0430

    app.use is using to apply middleware and i used morgan and bodyParser as middleware i also wireup nodemon on script

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
 
 const port = process.env.PORT || 3009;
 const server = http.createServer(app);
