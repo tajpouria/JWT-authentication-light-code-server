1dc1db5ae7d231678b1254e074f049a5652aa121
Pouria <charliswager@gmail.com>
2019-03-30 02:39:23 +0430

    try to finding same emails

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,8 @@
+const User = require("../models/user");
+
 exports.signup = function(req, res, nex) {
-  res.send({ success: "true" });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: email }, function(err, exisitingUser) {});
 };
