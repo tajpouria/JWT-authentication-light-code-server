72b9a97a67d21dc5e763722f6edc797551e9dccc
Pouria <charliswager@gmail.com>
2019-03-30 00:23:28 +0430

    setup simple routes file

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
+const routes = require("./routes");
 
 app.use(morgan("combined"));
 app.use(bodyParser.json({ type: "*/*" }));
+routes(app);
 
 const port = process.env.PORT || 3009;
 const server = http.createServer(app);
