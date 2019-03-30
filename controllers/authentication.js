592605e439f90455fc6b0170143aac295084e116
Pouria <charliswager@gmail.com>
2019-03-30 14:06:51 +0430

    check if there is no email or password

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,12 +4,16 @@ exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
+  if (!email || !password) {
+    return res.status(422).send("Email and password must be provided");
+  }
+
   User.findOne({ email: email }, function(err, exisitingUser) {
     if (err) return next(err);
     if (exisitingUser) {
       return res
         .status(422)
-        .send("Unprocessable Entity: The user with given email in use");
+        .send("Unprocessable Entity: The user with given email in use.");
     }
     const user = new User({
       email: email,
