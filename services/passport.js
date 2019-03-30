3e625bd55fd6094e240948443d43887dc44b9553
Pouria <charliswager@gmail.com>
2019-03-31 01:35:08 +0430

    to using jwt strategy in passport i done this steps : 1. setup jwtstrategy = new jwtstrategy opt callback payload, done    opt is we exrect data from decoded jwt and call back will call after it payload is jwt data and done is a function that we should receive user after finding the user we can send it as second arg of done function and it s firt arg is err

---
+++ services/passport.js
@@ -0,0 +1,21 @@
+const passport = require("passport");
+const User = require("../models/user");
+const config = require("../config");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const jwtOption = {};
+
+const jwtStarategy = new JwtStrategy(jwtOption, function(payload, done) {
+  User.findById(payload.sub, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
