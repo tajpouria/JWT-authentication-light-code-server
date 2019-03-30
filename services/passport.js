df2312f55c9e37816fc7510ed461c5f636abf9a9
Pouria <charliswager@gmail.com>
2019-03-31 02:29:20 +0430

    as i used it before the first arg of done function is err and second one is user

--- services/passport.js
+++ services/passport.js
@@ -10,7 +10,16 @@ const localLogin = new LocalStrategy(localOption, function(
   email,
   password,
   done
-) {});
+) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+  });
+});
 
 const jwtOption = {
   jwtFromRequest: ExtractJwt.fromHeader("authorization"),
