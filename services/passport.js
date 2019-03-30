f4d49a8944f46334583467046f20b80555f900bf
Pouria <charliswager@gmail.com>
2019-03-31 02:22:56 +0430

    localStrategy is used for verify email and password wire upr by new localStrategy firt arg is opt how extract email and password from body that by default it will used username and by usernamefield : email opt it will using email at body second arg is call back function that have email and password and done function

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,14 @@ const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+const localOption = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOption, function(
+  email,
+  password,
+  done
+) {});
 
 const jwtOption = {
   jwtFromRequest: ExtractJwt.fromHeader("authorization"),
