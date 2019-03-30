a92d74564990555e54ce7f0830e6a5f9ac124e91
Pouria <charliswager@gmail.com>
2019-03-30 14:42:20 +0430

    bcrypt setup in these steps  1. userSchema.pre "save" function actually this function will be called before save 2.bcypt.genSalt the function will execute after first arg given time and hash using to crypt the given data with salt and function called after that

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -7,6 +8,22 @@ const userSchema = new mongoose.Schema({
     unique: true
   },
   password: String
+});
+
+userSchema.pre("save", function() {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password(hash);
+      next();
+    });
+  });
 });
 
 const ModelClass = mongoose.model("user", userSchema);
